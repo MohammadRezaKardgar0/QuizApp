e568ad60c7a8567817aea3913780f6db0bffdefd
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-10-04 14:38:11 +0330

    change format data

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+import formatData from "./helper.js";
+
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 
@@ -9,7 +11,8 @@ let formattedData = null;
 const fetchData = async () => {
   const res = await fetch(URL);
   const json = await res.json();
-  formattedData = json;
+  formattedData = formatData(json.results);
+  console.log(formattedData);
   start();
 };
 
